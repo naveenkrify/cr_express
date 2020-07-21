288f8373d87eb3fa7ba1047b75a6d493550f75e8
naveenkrify <46560524+naveenkrify@users.noreply.github.com>
2020-07-21 19:15:07 +0530

    Add files via upload
    Day2

--- app.js
+++ app.js
@@ -16,13 +16,65 @@ var details = {
 
 app.get('/',function(req,res){
 
+    var name = 'sai'
+
 //res.send('Hello world')
-res.render('hello')
+res.render('body',{name1:name,age:25,sex:'male'})
+})
+
+
+//USERS DATA
+var users = [
+    {
+        id:1,
+        name:'naveen',
+        sex:'male',
+        age:25,
+        company:'krify'
+    },
+    {
+        id:2,
+        name:'sai',
+        sex:'male',
+        age:25,
+        company:'krify2'
+    },
+    {
+        id:3,
+        name:'chakri',
+        sex:'male',
+        age:25,
+        company:'krify3'
+    }
+]
+
+app.get('/users',function(req,res){
+    //res.send('showing all users')
+    
+    res.render('users',{users:users})
+})
+
+app.get('/user/:id',function(req,res){
+    var id = req.params.id
+    var user_;
+
+    users.forEach(function(user){
+        if(user.id === parseInt(id)){
+            console.log('going')
+            user_ = user;
+        }
+    })
+    res.render('user',user_)
+
 })
 
 app.get('/profile',function(req,res){
 //res.send('Hello My name is Naveen ')
 res.json(details)
+})
+
+app.get('/profile/sai',function(req,res){
+    res.send('Hello World this is sai')
 })
 
 app.listen(8001,function(){
